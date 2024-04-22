526d32c94c27c4fa379f604d087196cb85d4a889
tylermoroz <moroztyler@gmail.com>
2024-04-22 15:45:09 -0600

    Create HashMap class and hash(key) function.

--- project.js
+++ project.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+class HashMap {
+  constructor() {
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+    this.buckets = [];
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
