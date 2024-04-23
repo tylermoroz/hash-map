8b836bc1e0a6e93bf01a48eaa12a0beb7fee4558
tylermoroz <moroztyler@gmail.com>
2024-04-23 15:36:08 -0600

    Create set(key, value) and get(key) functions.

--- project.js
+++ project.js
@@ -7,6 +7,10 @@ class HashMap {
     this.buckets = [];
   }
 
+  print() {
+    console.log(this.buckets);
+  }
+
   hash(key) {
     let hashCode = 0;
 
@@ -17,4 +21,39 @@ class HashMap {
 
     return hashCode;
   }
+
+  set(key, value) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      this.buckets[index] = [[key, value]];
+    } else {
+      for (let i = 0; i < this.buckets[index].length; i++) {
+        if (this.buckets[index][i][0] === key) {
+          this.buckets[index][i][1] = value;
+        }
+      }
+    }
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      return null;
+    }
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) {
+        return this.buckets[index][i][1];
+      }
+    }
+  }
 }
+
+const newTable = new HashMap();
+newTable.set("dave", "smith");
+newTable.set("darryl", "dixon");
+newTable.set("tyler", "moroz");
+newTable.print();
+console.log(newTable.get("tyler"));
+console.log(newTable.get("darryl"));
+console.log(newTable.get("dave"));
+console.log(newTable.get("melissa"));
