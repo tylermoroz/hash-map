40a3e1cf2910effcc7341a7d033773b73076fe54
tylermoroz <moroztyler@gmail.com>
2024-04-26 14:25:57 -0600

    Create has(key), remove(key), length(), and clear() methods to the HashMap class.

--- project.js
+++ project.js
@@ -46,14 +46,54 @@ class HashMap {
       }
     }
   }
+
+  has(key) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      return false;
+    }
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) {
+        return true;
+      }
+    }
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      return false;
+    }
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) {
+        delete this.buckets[index];
+        return true;
+      }
+    }
+  }
+
+  length() {
+    return Object.keys(this.buckets).length;
+  }
+
+  clear() {
+    this.buckets = [];
+    return this.buckets;
+  }
 }
 
-const newTable = new HashMap();
-newTable.set("dave", "smith");
-newTable.set("darryl", "dixon");
-newTable.set("tyler", "moroz");
-newTable.print();
-console.log(newTable.get("tyler"));
-console.log(newTable.get("darryl"));
-console.log(newTable.get("dave"));
-console.log(newTable.get("melissa"));
+const table = new HashMap();
+table.set("dave", "smith");
+table.set("darryl", "dixon");
+table.set("tyler", "moroz");
+table.print();
+console.log(table.get("tyler"));
+console.log(table.get("darryl"));
+console.log(table.get("dave"));
+console.log(table.get("melissa"));
+console.log(table.has("tyler"));
+console.log(table.has("carter"));
+console.log(table.remove("tyler"));
+console.log(table.remove("carter"));
+console.log(table.length());
+console.log(table.clear());
