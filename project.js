da053147d26e43e09e5fbafbc2297882fac365d3
tylermoroz <moroztyler@gmail.com>
2024-04-26 15:30:37 -0600

    Create keys(), values(), and entries() methods to the HashMap class.

--- project.js
+++ project.js
@@ -80,6 +80,36 @@ class HashMap {
     this.buckets = [];
     return this.buckets;
   }
+
+  keys() {
+    let keys = [];
+    for (const key in this.buckets) {
+      for (let i = 0; i < this.buckets[key].length; i++) {
+        keys.push(this.buckets[key][i][0]);
+      }
+    }
+    return keys;
+  }
+
+  values() {
+    let values = [];
+    for (const key in this.buckets) {
+      for (let i = 0; i < this.buckets[key].length; i++) {
+        values.push(this.buckets[key][i][1]);
+      }
+    }
+    return values;
+  }
+
+  entries() {
+    let entries = [];
+    for (const entry in this.buckets) {
+      for (let i = 0; i < this.buckets[entry].length; i++) {
+        entries.push([this.buckets[entry][i][0], this.buckets[entry][i][1]]);
+      }
+    }
+    return entries;
+  }
 }
 
 const table = new HashMap();
@@ -96,4 +126,7 @@ console.log(table.has("carter"));
 console.log(table.remove("tyler"));
 console.log(table.remove("carter"));
 console.log(table.length());
-console.log(table.clear());
+// console.log(table.clear());
+console.log(table.keys());
+console.log(table.values());
+console.log(table.entries());
