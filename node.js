99a25a5a099e5b9fd8c48c8ddef26585acbed3c6
Tyler Moroz <moroztyler@gmail.com>
2024-06-02 16:54:01 -0600

    Remove unused methods from the LinkedList class.

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 // Export Node class for use in linked-list module
 export default class Node {
   constructor(value) {
-    this.value = value; // value that represents the Node
-    this.nextNode = null; // pointer for the next Node in the list
+    this.value = value;
+    this.nextNode = null;
   }
 }
